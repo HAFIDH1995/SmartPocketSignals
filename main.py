0e8db7d3d44de331e32dea74d69144eff40e8d6d
HAFIDH1995 <hafidhboudjerar@gmail.com>
2025-06-13 16:40:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "SmartPocketSignals is running!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
